f624e51bed8e19f9b08343707d0598d0f4cd5951
Tobias Brunner <tobias@wuala.com>
2015-01-12 16:44:48 +0100

    add authentication test

--- test/test.rb
+++ test/test.rb
@@ -6,5 +6,10 @@ describe 'GET /' do
     get '/'
     last_response.status.must_equal 401
   end
+  it 'should respond with authentication' do
+    authorize "deployer", "Deploy1T"
+    get '/'
+    assert last_response.ok?
+  end
 end
 

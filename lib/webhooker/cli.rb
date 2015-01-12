4f943a54d26cba6cd2907bca62d87b7c86331495
Tobias Brunner <tobias@wuala.com>
2015-01-12 10:07:33 +0100

    more robust application loading

--- lib/webhooker/cli.rb
+++ lib/webhooker/cli.rb
@@ -9,9 +9,15 @@ module Webhooker
     desc "start", "Starts the webhooker server"
     method_option :config_file, :desc => "Path to the configuration file"
     def start(*args)
+      begin
+        spec = Gem::Specification.find_by_name('webhooker')
+        gem_dir = spec.gem_dir
+      rescue Gem::LoadError
+        gem_dir = '.'
+      end
       port_option = args.include?('-p') ? '' : ' -p 8088'
       args = args.join(' ')
-      command = "bundle exec thin -R config.ru start#{port_option} #{args}"
+      command = "thin -R #{gem_dir}/config.ru start#{port_option} #{args}"
       command.prepend "export CONFIG_FILE=#{options[:config_file]}; " if options[:config_file]
       begin
         run_command(command)
@@ -23,7 +29,7 @@ module Webhooker
 
     desc "stop", "Stops the thin server"
     def stop
-      command = "bundle exec thin stop"
+      command = "thin -R #{spec.gem_dir}/config.ru stop"
       run_command(command)
     end
 

4637b201b810026f072e6fecb93a55176b1d05ae
Tobias Brunner <tobias@wuala.com>
2015-01-15 10:23:13 +0100

    fix test to match new log message

--- test/test_bitbucket.rb
+++ test/test_bitbucket.rb
@@ -30,7 +30,7 @@ describe 'Bitbucket payload' do
     authorize user, pass
     post '/payload/bitbucket', 'payload' => '%7B%22repository%22%3A %7B%22website%22%3A %22%22%2C %22fork%22%3A false%2C %22name%22%3A %22encdata%22%2C %22scm%22%3A %22git%22%2C %22owner%22%3A %22theowner%22%2C %22absolute_url%22%3A %22%2Ftheowner%2Fencdata%2F%22%2C %22slug%22%3A %22encdata%22%2C %22is_private%22%3A true%7D%2C %22truncated%22%3A false%2C %22commits%22%3A %5B%7B%22node%22%3A %22634d71461df8%22%2C %22files%22%3A %5B%7B%22type%22%3A %22modified%22%2C %22file%22%3A %22hooktest.txt%22%7D%5D%2C %22raw_author%22%3A %22Tobias Brunner %3Ctobias%40domain.com%3E%22%2C %22utctimestamp%22%3A %222014-12-03 16%3A04%3A36%2B00%3A00%22%2C %22author%22%3A %22tobru%22%2C %22timestamp%22%3A %222014-12-03 17%3A04%3A36%22%2C %22raw_node%22%3A %22634d71461df82f2095f18955cf967b606cb25f84%22%2C %22parents%22%3A %5B%22d94b7645874e%22%5D%2C %22branch%22%3A %22unknownbranch%22%2C %22message%22%3A %22hook test 3%5Cn%22%2C %22revision%22%3A null%2C %22size%22%3A -1%7D%5D%2C %22canon_url%22%3A %22https%3A%2F%2Fbitbucket.org%22%2C %22user%22%3A %22tobru%22%7D'
     assert_equal 500, last_response.status
-    assert last_response.body.must_match "no configuration for branch 'unknownbranch' found"
+    assert last_response.body.must_match "no configuration for branch 'unknownbranch' in repository 'encdata' found"
   end
   # payload has a unknown repository
   it 'should output 500 the repository unknownrepo is not configured' do

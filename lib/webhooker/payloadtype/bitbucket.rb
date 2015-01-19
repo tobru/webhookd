f3aba8cf83f4f05631caa040b34ed8e06d62d0cc
Tobias Brunner <tobias@wuala.com>
2015-01-19 11:07:01 +0100

    loop through bitbucket commits to find branch
    The bitbucket payload contains several commits. Not every commit
    has a branch name attached (why?). So we are looping through all
    loops until we find a branch name and use this one.

--- lib/webhooker/payloadtype/bitbucket.rb
+++ lib/webhooker/payloadtype/bitbucket.rb
@@ -16,12 +16,23 @@ module Webhooker
       json_parsed = JSON.parse(URI.unescape(prepared))
       logger.debug "raw received data: #{json_parsed}"
 
+      # loop through commits to find the branch
+      branch_name = '_notfound'
+      author_name = '_notfound'
+      json_parsed['commits'].each do |commit|
+        if commit['branch']
+          branch_name = commit['branch']
+          author_name = commit['author']
+          break
+        end
+      end
+
       data = Hash.new
       data[:type] = 'vcs'
       data[:source] = 'bitbucket'
       data[:repo_name] = json_parsed['repository']['name']
-      data[:branch_name] = json_parsed['commits'][0]['branch']
-      data[:author_name] = json_parsed['commits'][0]['author']
+      data[:branch_name] = branch_name
+      data[:author_name] = author_name
 
       logger.debug "parsed from the bitbucket data: #{data}"
 
